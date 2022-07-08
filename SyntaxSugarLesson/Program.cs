08e42ef6dfeef5bbe43282da7fc7472161dd671f
dannypellatt <danielpellatt92@gmail.com>
2022-07-08 10:24:05 -0400

    Initial commit

---
+++ SyntaxSugarLesson/Program.cs
@@ -0,0 +1,33 @@
+﻿// inferred typing
+
+var answer = 4;
+string response;
+if (answer < 9)
+{
+    response = answer + " is less than nine";
+}
+else
+{
+     response = answer + " is greater than or equal to nine";
+}
+
+
+// string interpolation
+
+int answer2 = 4;
+string response2;
+if (answer < 9)
+{
+    response2 = ($"{answer2} is less than nine");
+}
+else
+{
+    response2 = ($"{ answer} is greater than or equal to nine");
+}
+
+
+
+// ternary operators
+
+int answer3 = 4;
+string response3 = (answer3 < 9) ? $"{answer3} is less than nine" : $"{answer3} is greater than or equal to nine";
